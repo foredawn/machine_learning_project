6110d38a617fbf5e4a2b680af12d756362a32749
hxy <717844067@qq.com>
2017-08-30 10:05:58 +0800

    first modification

--- poi_id.py
+++ poi_id.py
@@ -16,6 +16,18 @@ features_list = ['poi','total_payments','bonus','expenses',
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+    
+# explore_data
+'''
+len(data_dict)
+len(data_dict["SKILLING JEFFREY K"])
+# 有多少个人是POI？
+n = 0
+for i in data_dict:
+    if data_dict[i]["poi"]==1:
+        n=n+1
+print n
+'''
 
 ### Task 2: Remove outliers
 data_dict.pop('TOTAL',0)
@@ -47,6 +59,7 @@ features = scaler.fit_transform(features)
 '''
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+clf.fit(features_train,labels_train)
 # Accuracy: 0.85880       Precision: 0.44843      Recall: 0.25650
 '''
 
@@ -54,6 +67,7 @@ clf = GaussianNB()
 '''
 from sklearn.neighbors import KNeighborsClassifier
 clf = KNeighborsClassifier(weights='distance')
+# clf.fit(features_train,labels_train)
 # Accuracy: 0.87993       Precision: 0.64741      Recall: 0.21850
 '''
 
@@ -64,12 +78,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.grid_search import GridSearchCV
 parameters = {'criterion':('gini', 'entropy'),
               'splitter':('best', 'random'),
-              'min_samples_split':[4,5]}
+              'min_samples_split':[3,4,5],
+              'random_state':[0]}
 dt = DecisionTreeClassifier()
 clf = GridSearchCV(dt, parameters)
+#clf.fit(features_train,labels_train)
 # Accuracy: 0.83593       Precision: 0.36189      Recall: 0.30200
 #'''
-
 
 
 
@@ -105,8 +120,6 @@ for train_idx, test_idx in cv:
         features_test.append( features[jj] )
         labels_test.append( labels[jj] )
         
-clf.fit(features_train,labels_train)
-
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -114,6 +127,7 @@ pred = clf.predict(features_test)
 acc = accuracy_score(labels_test,pred)
 ave = precision_score(labels_test,pred)
 rec = recall_score(labels_test,pred)
+
 '''
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
@@ -122,4 +136,5 @@ rec = recall_score(labels_test,pred)
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
 
